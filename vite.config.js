ab2ef520d86f47b55c6cc83f3c09d507aeb05922
Smit Mewada <smitmewada009@gmail.com>
2025-12-11 02:06:10 -0500

    Config change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    global: 'globalThis',  // Polyfill 'global' for some libs
+  },
+  resolve: {
+    alias: {
+      // Map Node.js modules to browser versions
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+      util: 'util',
+      events: 'events',
+    },
+  },
+  optimizeDeps: {
+    include: ['stream-browserify', 'buffer', 'util', 'events'],  // Pre-bundle for dev
+  },
+});
